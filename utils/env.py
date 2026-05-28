3113a15ebacc99a70f076ab617371d819719f470
Jiaqi Han <jiaqihan@stanford.edu>
2026-05-28 23:45:04 +0800

    init

---
+++ utils/env.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+HF_REPO_ID = "Goodeat/drifting"
+HF_ROOT = '/path/to/drifting_hf_root'
+VAE_HF_PATH = '/path/to/sdvae_hf_root'
+TORCH_HUB_DIR = "/path/to/torch_hub"
+
+WFLOW_HF_REPO_ID = "jiaqihan99/W-Flow"
+WFLOW_HF_ROOT = "/path/to/wflow_hf_root"
+
+IMAGENET_PATH = "/path/to/imagenet-1k"
+IMAGENET_CACHE_PATH = "/path/to/imagenet256-latents-sdvae"
+IMAGENET_FID_NPZ = "/path/to/jit_in256_stats.npz"
+IMAGENET_PR_NPZ = "/path/to/imagenet_val_prc_arr0.npz"  # or simply leave empty
